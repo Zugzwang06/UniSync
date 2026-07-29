3bdf940fa445357ae52ab1435c1d62dd981daa8e
Tejas2006 <sayhi2tejas@gmail.com>
2026-07-29 14:46:41 -0500

    Remove deprecated afterSignOutUrl prop from UserButton

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
               </button>
             </SignInButton>
           ) : (
-            <UserButton afterSignOutUrl="/" />
+            <UserButton />
           )}
 
         </div>
